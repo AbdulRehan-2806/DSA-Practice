39c4062a032d8394a8eebfac9ca803007c1deca1
AbdulRehan-2806 <rehanmohd9392@gmail.com>
2026-06-13 12:08:59 +0530

    Time: 2 ms (96.85%), Space: 46.6 MB (49.79%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        StringBuilder sb = new StringBuilder("");
+        for(String s : words)
+            {
+                int sum = 0;
+                for(char c : s.toCharArray())
+                    {
+                        sum += weights[c-'a'];
+                    }
+                sum %= 26;
+                sb.append((char)('z'-sum));
+            }
+        return sb.toString();
+    }
+}
