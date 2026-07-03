5695b5453d3bb6541c033f10cb28d14ea5ce640a
AbdulRehan-2806 <rehanmohd9392@gmail.com>
2026-07-03 20:40:29 +0530

    Time: 8 ms (55.66%), Space: 46.3 MB (84.24%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -2,27 +2,17 @@ class Solution {
     public List<String> topKFrequent(String[] words, int k) {
         int n = words.length;
         HashMap<String,Integer> hm = new HashMap<>();
-        List<String> al = new ArrayList<>();
         for(String s : words)
         {
-            al.add(s);
             hm.put(s,hm.getOrDefault(s,0)+1);
         }
+        List<String> al = new ArrayList<>(hm.keySet());
         Collections.sort(al,(a,b)->{
             int fa = hm.get(a);
             int fb = hm.get(b);
             if(fa != fb) return Integer.compare(fb,fa);
             return a.compareTo(b);
         });
-        List<String> ans = new ArrayList<>();
-        ans.add(al.get(0));
-        int i=1,j=1;
-        while(j<k)
-        {
-            while(ans.get(j-1).equals(al.get(i))) i++;
-            ans.add(al.get(i));
-            j++;
-        }
-        return ans;
+        return al.subList(0,k);
     }
 }
