1f91391b89d152842b20348758c92ca19448d23b
AbdulRehan-2806 <rehanmohd9392@gmail.com>
2026-07-26 11:26:25 +0530

    Time: 99 ms (14.6%), Space: 207.3 MB (5.43%) - LeetHub

---
+++ 4001-aggregate-two-time-series/4001-aggregate-two-time-series.java
@@ -0,0 +1,51 @@
+class Solution {
+    public List<List<Integer>> aggregateTimeSeries(int[][] series1, int[][] series2) {
+        int n = series1.length;
+        int m = series2.length;
+        int min = Math.min(series1[0][0] , series2[0][0]);
+        int max = Math.max(series1[n-1][0] , series2[m-1][0]);
+        TreeSet<Integer> list = new TreeSet<>();
+        for(int[] arr : series1)
+        {
+            int t = arr[0];
+            list.add(t);
+        }
+        for(int[] arr : series2)
+        {
+            int t = arr[0];
+            list.add(t);
+        }
+        List<List<Integer>> ans = new ArrayList<>();
+        int j = 0 , k = 0;
+        for(int i : list)
+        {
+            int t1 = 0 , t2 = 0 , val1 = 0 , val2 = 0;
+            if(j>=n)
+            {
+                t1 = i;
+                val1 = 0;
+            }
+            else{
+                t1 = series1[j][0];
+                val1 = series1[j][1];
+            }
+            if(k>=m)
+            {
+                t2 = i;
+                val2 = 0;
+            }
+            else{
+                t2 = series2[k][0];
+                val2 = series2[k][1];
+            }
+            int sum = val1+val2;
+            List<Integer> al = new ArrayList<>();
+            al.add(i);
+            al.add(sum);
+            ans.add(al);
+            if(t1 == i) j++;
+            if(t2 == i) k++;
+        }
+        return ans;
+    }
+}
